65e9988ae839ab4dee580bee5055fd1460521cf2
vicky <pratamavr@gmail.com>
2026-04-09 22:15:28 +0800

    updates

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Modules\SwiftBank\Http\Controllers\SwiftBankController;
 
 Route::middleware(['auth:sanctum'])->prefix('swift')->name('swift.')->group(function () {
-  Route::apiResource('countries', [SwiftBankController::class, 'countries'])->name('countries');
-  Route::apiResource('banks/{countryCode}', [SwiftBankController::class, 'banks'])->name('banks');
+  Route::get('countries', [SwiftBankController::class, 'countries'])->name('countries');
+  Route::get('banks/{countryCode}', [SwiftBankController::class, 'banks'])->name('banks');
 });
